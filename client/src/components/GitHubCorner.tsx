7f8c4ca140c364f3bd01e0bfb73b1731bf892947
vaibhav486 <vaibhav.ug23.cs@nitp.ac.in>
2025-07-02 07:48:42 +0530

    GitHubCorner.tsx

--- client/src/components/GitHubCorner.tsx
+++ client/src/components/GitHubCorner.tsx
@@ -7,7 +7,7 @@ function GitHubCorner() {
 
     return (
         <a
-            href="https://github.com/sahilatahar/Code-Sync"
+            href="https://github.com/vaibhav486/code-sync"
             className="github-corner"
             aria-label="View source on GitHub"
             target="_blank"
